dcd25e2d1008fbb384f2625d2477f9f53b989d42
David <dmg_92@hotmail.com>
2024-01-05 22:25:51 -0600

    listo

--- core/utils.py
+++ core/utils.py
@@ -1,23 +1,24 @@
 import requests
 from datetime import datetime
+from django.conf import settings
 
 def get_places_reservamos(query):
-    url = f"https://search.reservamos.mx/api/v2/places?q={query}"
+    url = "{}/places?q={}".format(settings.RESERVAMOS_API, query)
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Lanza una excepción para errores HTTP
+        response.raise_for_status()
         return response.json()
     except requests.exceptions.RequestException as e:
         print(f"Error en la solicitud a Reservamos API: {e}")
         return []
 
 def get_weather_data(lat, long):
-    api_key = "a5a47c18197737e8eeca634cd6acb581"
-    url = f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={long}&exclude=current,minutely,hourly&appid={api_key}"
+    url = "{}?lat={}&lon={}&exclude=current,minutely,hourly&appid={}".format(
+        settings.OPEN_WEATHER_API, lat, long, settings.OPEN_WEATHER_API_KEY
+    )
     try:
         response = requests.get(url)
         response.raise_for_status()
-        print(type(response.json()))
         return response.json()
     except requests.exceptions.RequestException as e:
         print(f"Error en la solicitud a OpenWeather API: {e}")
--- reservamos_challenge/settings.py
+++ reservamos_challenge/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+RESERVAMOS_API = config('RESERVAMOS_API')
+OPEN_WEATHER_API = config('OPEN_WEATHER_API')
+OPEN_WEATHER_API_KEY = config('OPEN_WEATHER_API_KEY')
